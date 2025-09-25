f8e104bbd0e56e12688a46903a685078ae75513c
Alexander Guttenberger <alex.guttenberger50@gmail.com>
2025-09-25 22:23:00 +0200

    add my dotfiles

--- dot_config/nvim/lua/plugins/oxocarbon.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "nyoom-engineering/oxocarbon.nvim",
-  config = function()
-    vim.opt.background = "dark"
-    vim.cmd.colorscheme "oxocarbon"
-  end,
-  priority = 1000,
-  lazy = false
-}
--- run_once_install_ansible.sh
+++ run_once_install_ansible.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-install_on_fedora() {
-    sudo dnf install -y ansible
-}
-
 install_on_ubuntu() {
     sudo apt-get update
     sudo apt-get install -y ansible
@@ -16,9 +12,7 @@ install_on_mac() {
 OS="$(uname -s)"
 case "${OS}" in
     Linux*)
-        if [ -f /etc/fedora-release ]; then
-            install_on_fedora
-        elif [ -f /etc/lsb-release ]; then
+        if [ -f /etc/lsb-release ]; then
             install_on_ubuntu
         else
             echo "Unsupported Linux distribution"
@@ -33,9 +27,6 @@ case "${OS}" in
         exit 1
         ;;
 esac
-
-
-ansible-playbook ~/.bootstrap/setup.yml --ask-become-pass
 
 echo "Ansible installation complete."
 
